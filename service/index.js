4b135cec6323db597ac29c1635c5009c3c7d8944
Michael Malura <maluramichael@googlemail.com>
2016-10-26 21:37:10 +0200

    Add first simple service generator

--- service/index.js
+++ service/index.js
@@ -3,17 +3,34 @@ var generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
     constructor: function() {
         generators.Base.apply(this, arguments);
+        this.answers = {};
     },
-    prompting: function() {
+    paths: function() {
+        console.log(this.destinationRoot());
+        console.log(this.destinationPath('index.js'));
+        console.log(this.sourceRoot());
+        console.log(this.templatePath('index.js'));
+    },
+    promptings: function() {
         return this.prompt([
             {
                 type: 'input',
                 name: 'name',
                 message: 'Your service name',
-                default: 'Foo' // Default to current folder name
+                default: 'Foo',
+                store: true
             }
         ]).then(function(answers) {
-            this.log('service name', answers.name);
+            this.answers = answers;
         }.bind(this));
+    },
+    writing: function() {
+        console.log(this.answers);
+        this.fs.copyTpl(
+          this.templatePath('service.js'),
+          this.destinationPath('app/services/' + this.answers.name + '/index.js'), {
+            name: this.answers.name
+          }
+        );
     }
 });
---
+++ service/templates/service.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @class <%= name %>
+ */
+class <%= name %> {
+	constructor() {
+
+  }
+}
+
+export default new <%= name %>();
